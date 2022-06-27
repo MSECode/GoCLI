d4e0df9b00f1a2854355ad93cb094885a52498ab
MSECode <jacopolosi33@gmail.com>
2022-06-27 23:06:16 +0200

    Write and read simple commands

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
+
+var (
+	writeNote string
+	readNote  string
+)
+
+func init() {
+	flag.StringVar(&writeNote, "w", "note", "The type of note to be written. Possible types: note, todo, list")
+	flag.StringVar(&readNote, "r", "note", "The type of note to be read. Possible types: note, todo, list")
+}
 
 func main() {
-	fmt.Print("Hello World!!\n")
+
+	flag.Parse()
+
+	if writeNote != "" { //default case
+		var title string
+		fmt.Printf("You have choose to write a %s. Type the title:\n", writeNote)
+		fmt.Scanln(&title)
+		fmt.Printf("This is your title: %s\n", title)
+	}
 }
